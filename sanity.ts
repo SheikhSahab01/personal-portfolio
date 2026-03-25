4860300363d1568c0846be5d834c0b70f5564e56
Mohd Asif Sheikh <63442347+SheikhSahab01@users.noreply.github.com>
2026-03-25 17:32:06 +0530

    Update sanity.ts

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,8 @@ export const config = {
     dataset : process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "byu9vdij",
     apiVersion : "2024-07-24",
-    useCdn : process.env.NODE_ENV === "production",
+    // useCdn : process.env.NODE_ENV === "production",
+    useCdn : process.env.NEXT_PUBLIC_SANITY_USE_CDN === "true",
 }
 
 export const sanityClient = createClient(config); 
